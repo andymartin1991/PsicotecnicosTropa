a0491ed1cbe5c0bd6b758d25dd7949379115496d
andymartin1991 <andymartin1991@gmail.com>
2017-04-26 08:56:06 +0200

    prueba desde trabajo

---
+++ app/src/main/java/com/example/andym/psicotecnicostropa/main_contacto.java
@@ -0,0 +1,18 @@
+package com.example.andym.psicotecnicostropa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xe63008 on 26/04/2017.
+ */
+
+public class main_contacto extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_contacto);
+
+
+    }
+
+}
