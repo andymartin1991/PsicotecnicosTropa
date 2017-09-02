5f64080c0a49fbb8ef82477b838c250d9685eb9c
andymartin1991 <andymartin1991@gmail.com>
2017-09-02 20:44:58 +0200

    añadido funcionalidades nuevas -- andres

--- app/src/main/java/com/example/andym/psicotecnicostropa/main_principal.java
+++ app/src/main/java/com/example/andym/psicotecnicostropa/main_principal.java
@@ -1,22 +1,35 @@
 package com.example.andym.psicotecnicostropa;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class main_principal extends Activity {
+
+    int textlength = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_principal);
 
-        final Button fatiga, aleatorio, trucos, contacta, examen, estudio, foro, facebook, twiter, instagran, informacion, evolucion, calcular, ayuda, pruebafisica, compartir, votar;
+        final Button fatiga, aleatorio, trucos, contacta, examen, estudio, foro, facebook, twiter, instagran, informacion, evolucion, calcular, ayuda, pruebafisica, compartir, votar, patatas, academia;
 
         fatiga = (Button) findViewById(R.id.button1);
         aleatorio = (Button) findViewById(R.id.button2);
@@ -35,6 +48,8 @@ public class main_principal extends Activity {
         pruebafisica = (Button) findViewById(R.id.button25);
         compartir = (Button) findViewById(R.id.button31);
         votar = (Button) findViewById(R.id.button32);
+        patatas = (Button) findViewById(R.id.button32);
+        academia = (Button) findViewById(R.id.button6);
 
         final boolean[] entra = {true};
         estudio.setOnClickListener(new View.OnClickListener() {
@@ -451,6 +466,143 @@ public class main_principal extends Activity {
                 }
             }
         });
+
+        patatas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (entra[0] == true) {
+                    entra[0] = false;
+                    Animation animation = AnimationUtils.loadAnimation(
+                            getApplicationContext(), R.anim.rotar);
+                    patatas.startAnimation(animation);
+                    new Thread(new Runnable() {
+                        public void run() {
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                            }
+                            Intent intent = new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("https://patatasarmadas.sistec.es/"));
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                    | Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+                            startActivity(intent);
+                            entra[0] = true;
+                        }
+                    }).start();
+                }
+            }
+        });
+
+
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(main_principal.this);
+        builder
+                .setIcon(getResources().getDrawable(R.drawable.iexc))
+                .setTitle(getString(R.string.atencion))
+                .setMessage(getString(R.string.atenacademia))
+                .setCancelable(false)
+                .setNegativeButton("Salir", null)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @SuppressWarnings("deprecation")
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //AlertDialog.Builder builder = new AlertDialog.Builder(main_principal.this);
+                        final LayoutInflater inflater = main_principal.this.getLayoutInflater();
+                        final View textEntryView = inflater.inflate(R.layout.loggin, null);
+                        final EditText correo = (EditText) textEntryView.findViewById(R.id.correo);
+                        final EditText password = (EditText) textEntryView.findViewById(R.id.password);
+                        final EditText search = (EditText) textEntryView.findViewById(R.id.search);
+                        final ListView academias = (ListView) textEntryView.findViewById(R.id.academias);
+
+                        final String listview_array[] = { "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN" };
+                        final ArrayList<String> array_sort = new ArrayList<String>();
+                        academias.setVisibility(View.GONE);
+
+                        academias.setAdapter(new ArrayAdapter<String>(main_principal.this,
+                                android.R.layout.simple_list_item_1, listview_array));
+                        search.addTextChangedListener(new TextWatcher() {
+                            public void afterTextChanged(Editable s) {
+
+                            }
+
+                            public void beforeTextChanged(CharSequence s, int start, int count,
+                                                          int after) {
+                                // Abstract Method of TextWatcher Interface.
+                            }
+
+                            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                                textlength = search.getText().length();
+                                array_sort.clear();
+
+                                for (int i = 0; i < listview_array.length; i++) {
+                                    if (textlength <= listview_array[i].length()) {
+                                        if (search.getText().toString().equalsIgnoreCase((String) listview_array[i].subSequence(0, textlength))) {
+                                            array_sort.add(listview_array[i]);
+                                        }
+                                    }
+                                }
+
+                                academias.setAdapter(new ArrayAdapter<String>(main_principal.this, android.R.layout.simple_list_item_1, array_sort));
+                                if(textlength == 0){
+                                    academias.setVisibility(View.GONE);
+                                }else{
+                                    academias.setVisibility(View.VISIBLE);
+                                }
+                            }
+                        });
+
+                        AlertDialog.Builder builder2 = new AlertDialog.Builder(main_principal.this);
+                        builder2.setView(textEntryView);
+                        builder2.setIcon(getResources().getDrawable(R.drawable.iexc));
+                        builder2.setTitle(getString(R.string.atencion));
+                        builder2.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                String Correo	=  correo.getText().toString();
+                                String Password = password.getText().toString();
+
+
+                            }
+                        });
+                        builder2.setNegativeButton("Salir", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                            }
+                        }).create().show();
+                    }
+                }
+        );
+
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                builder.create().show();
+            }
+        };
+        academia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (entra[0] == true) {
+                    entra[0] = false;
+                    Animation animation = AnimationUtils.loadAnimation(
+                            getApplicationContext(), R.anim.rotar);
+                    academia.startAnimation(animation);
+                    final Handler handler = new Handler();
+                    new Thread(new Runnable() {
+                        public void run() {
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                            }
+                            handler.post(runnable);
+                            entra[0] = true;
+                        }
+                    }).start();
+                }
+            }
+        });
+
+
+
+
 
     }
 }
